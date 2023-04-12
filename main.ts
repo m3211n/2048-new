1ae961c03c123bc0853e1a5d868ca93cec835b91
m3211n <130044007+m3211n@users.noreply.github.com>
2023-04-12 18:59:22 +0200

    all works but need to win and lose

--- main.ts
+++ main.ts
@@ -14,7 +14,83 @@ function add1 () {
 
 function shiftTiles (direction: number) {
 
+    let nextIndex = 0                                               // index to get the number at from the main array
+
+    let segment: number[] = []
+
+    isModified = false                                              // array is not modified at the beginning
+
+    // step through every segment, vertical or horizontal depending on direction
+    for (let i = 0; i <= 3; i++) {                                  
+        segment = [0, 0, 0, 0]                                      
+        
+        // get segment for specified index
+        for (let j = 0; j <= 3; j++) {
+            if (direction == _LEFT || direction == _RIGHT) {
+                nextIndex = i * 4 + j
+            } else {
+                nextIndex = j * 4 + i
+            }
+            segment[j] = tilesNumbers[nextIndex]
+        }
+
+        // reverse the segment if it was swiped right or down
+        if (direction == _RIGHT || direction == _DOWN) {
+            segment.reverse()
+        }
+
+        // do the optimization of the segment
+        let i0 = 0
+        let i1 = 1
+        for (let index = 0; index < 3; index++) {
+            if (segment[i0] != 0) {
+                if (segment[i1] != 0) {
+                    if (segment[i0] == segment[i1]) {
+                        segment[i0] = segment[i0] + 1
+                        segment.removeAt(i1)
+                        segment.push(0)
+                    }
+                    i0 += 1
+                    i1 += 1
+                } else {
+                    segment.removeAt(i1)
+                    segment.push(0)
+                }
+            } else {
+                segment.removeAt(i0)
+                segment.push(0)
+            }
+        }
+
+        // reverse back
+        if (direction == _RIGHT || direction == _DOWN) {
+            segment.reverse()
+        }
+
+        // put segment back to the main array and go to the next segment
+        for (let j = 0; j <= 3; j++) {
+            if (direction == _LEFT || direction == _RIGHT) {
+                nextIndex = i * 4 + j
+            } else {
+                nextIndex = j * 4 + i
+            }
+
+            // verify if new numbers differs from the original. If so, consider the main array modified 
+            if (segment[j] != tilesNumbers[nextIndex]) {
+                tilesNumbers[nextIndex] = segment[j]
+                isModified = true
+            }
+        }
+    }
+
+    // if the array was modified (at least one of segments was changed) then we need to seed new tile with "1" and tiles has to be re-drawn 
+    if (isModified) {
+        add1()
+        drawTiles()
+    }
 }
+
+/*
 
 function optimizeBufferList () {
     let i0 = 0
@@ -40,24 +116,28 @@ function optimizeBufferList () {
             bufferList.push(0)
         }
     }
-    isOptimized = false
+    isModified = false
     for (let i = 0; i < bufferList.length; i++) {
         if (bufferList[i] != bufferSnapshot[i]) {
-            isOptimized = true
+            isModified = true
         }
     }
 }
+
+*/
 
 function drawTiles () {
     let x = 0
     let y = 0
     for (let i = 0; i <= 15; i++) {
-        x = x_start + i % 4 * 24
-        y = y_start + Math.floor(i / 4) * 24
+        x = x_start + i % 4 * 30
+        y = y_start + Math.floor(i / 4) * 30
         tilesSprites[i].setImage(tilesImages[tilesNumbers[i]])
         tilesSprites[i].setPosition(x, y)
     }
 }
+
+/*
 
 function getBufferList (position: number, isRow: boolean) {
     bufferList = [0, 0, 0, 0]
@@ -80,67 +160,28 @@ function putBufferList (position: number, isRow: boolean) {
     }
 }
 
+*/
+
 controller.left.onEvent(ControllerButtonEvent.Pressed, function() {
-    isOptimized = false
-    for (let i = 0; i <= 3; i++) {
-        getBufferList(i, true)
-        optimizeBufferList()
-        putBufferList(i, true)
-    }
-    if (isOptimized) {
-        add1()
-        drawTiles()
-    }
+    shiftTiles(_LEFT)
 })
 
 controller.right.onEvent(ControllerButtonEvent.Pressed, function() {
-    isOptimized = false
-    for (let i = 0; i <= 3; i++) {
-        getBufferList(i, true)
-        bufferList.reverse()
-        optimizeBufferList()
-        bufferList.reverse()
-        putBufferList(i, true)
-    }
-    if (isOptimized) {
-        add1()
-        drawTiles()
-    }
+    shiftTiles(_RIGHT)
 })
 
 controller.up.onEvent(ControllerButtonEvent.Pressed, function() {
-    isOptimized = false
-    for (let i = 0; i <= 3; i++) {
-        getBufferList(i, false)
-        optimizeBufferList()
-        putBufferList(i, false)
-    }
-    if (isOptimized) {
-        add1()
-        drawTiles()
-    }
+    shiftTiles(_UP)
 })
 
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    isOptimized = false
-    for (let i = 0; i <= 3; i++) {
-        getBufferList(i, false)
-        bufferList.reverse()
-        optimizeBufferList()
-        bufferList.reverse()
-        putBufferList(i, false)
-    }
-    if (isOptimized) {
-        add1()
-        drawTiles()
-    }
+    shiftTiles(_DOWN)
 })
 
-//let i2 = 0
-//let i1 = 0
-//let i0 = 0
-//let newList: number[] = []
-//let testList: number[] = []
+const _UP = 0                // direction constants
+const _RIGHT = 3
+const _DOWN = 6
+const _LEFT = 9
 
 let _top = 0
 let _right = 3
@@ -175,12 +216,11 @@ for (let index = 0; index < 16; index++) {
     tilesSprites.push(sprites.create(tilesImages[0], SpriteKind.Tile))
 }
 
-let bufferList: number[] = []
-let isOptimized = false
+let isModified = false
 let zerosIndexes: number[] = []
 
-let x_start = 44
-let y_start = 24
+let x_start = 35
+let y_start = 15
 
 add1()
 add1()
